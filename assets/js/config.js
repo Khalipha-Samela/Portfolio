7b1af555c979dba1396e799d17996f38081d9eed
Your Name <you@example.com>
2026-03-17 22:29:27 +0200

    Delete config.js

--- assets/js/config.js
+++
@@ -1,7 +0,0 @@
-// ADD THIS TO .gitignore!
-const EMAILJS_CONFIG = {
-    PUBLIC_KEY: 'I8ay9X62lCnok6pOQ',
-    SERVICE_ID: 'service_wwb706n',
-    TEMPLATE_ID: 'template_2uzzv1n',
-    AUTO_REPLY_TEMPLATE_ID: 'your_auto_reply_template_id' // optional
-};
