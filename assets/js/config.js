d1fe66c432c52fa2697e466db9ae103221679b1e
Your Name <you@example.com>
2026-04-08 19:13:32 +0200

    Add config.js for EmailJS

---
+++ assets/js/config.js
@@ -0,0 +1,6 @@
+const EMAILJS_CONFIG = {
+    PUBLIC_KEY: 'I8ay9X62lCnok6pOQ',
+    SERVICE_ID: 'service_wwb706n',
+    TEMPLATE_ID: 'template_2uzzv1n',
+    AUTO_REPLY_TEMPLATE_ID: 'your_auto_reply_template_id' // optional
+};
